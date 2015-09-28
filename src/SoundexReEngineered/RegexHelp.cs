3b5ccd70df89123e42db3670d4b0098394bc0e1b
Sanket Joshi <sanketjoshi_2005@rediffmail.com>
2015-09-28 21:14:47 +0530

    Added check for ish and ush
    Added check for ish and ush
    to work with names like "Krishna" or "Krushna" and "Rishi" or "Rushi"

--- src/SoundexReEngineered/RegexHelp.cs
+++ src/SoundexReEngineered/RegexHelp.cs
@@ -10,15 +10,11 @@ namespace SoundexCore
     public static class RegexHelp
     {
         //Regex to find three letters match, two letters match, one letter match
-        public static Regex regTriples = new Regex(@"\Baou\B|\Biya\B|
-                                                     \Baou\b|\Biya\b|
-                                                     \baou\B|\biya\B|
-                                                     \baou\b|\biya\b|
-                                                     \Bder\b|\Bdra\b");
-        public static Regex regDoubles = new Regex(@"\Baa\B|\Bou\B|\Bau\B|\Baw\B|\Bav\B|\Bow\B|\Bov\B|\Bee\B|\Bii\B|\Bea\B|\Bei\B|\Bai\B|\Bph\B|\Bsh\B|\Bia\B|\Bck\B|\Buv\B
-                                                     \Baa\b|\Bou\b|\Bau\b|\Baw\b|\Bav\b|\Bow\b|\Bov\b|\Bee\b|\Bii\b|\Bea\b|\Bei\b|\Bai\b|\Bph\b|\Bsh\b|\Bia\b|\Bck\b|\Buv\b
-                                                     \baa\B|\bou\B|\bau\B|\baw\B|\bav\B|\bow\B|\bov\B|\bee\B|\bii\B|\bea\B|\bei\B|\bai\B|\bph\B|\bsh\B|\bia\B|\bck\B|\buv\B
-                                                     \baa\b|\bou\b|\bau\b|\baw\b|\bav\b|\bow\b|\bov\b|\bee\b|\bii\b|\bea\b|\bei\b|\bai\b|\bph\b|\bsh\b|\bia\b|\bck\b|\buv\b");
+        public static Regex regTriples = new Regex(@"\Baou\B|\Biya\B|\Baou\b|\Biya\b|\baou\B|\biya\B|\baou\b|\biya\b|\Bish\B|\Bish\b|\bish\B|\bish\b|\bush\B|\bush\b|\Bush\b|\Bush\B|\Bder\b|\Bdra\b");
+        public static Regex regDoubles = new Regex(@"\Baa\B|\Bou\B|\Bau\B|\Baw\B|\Bav\B|\Bow\B|\Bov\B|\Bee\B|\Bii\B|\Bea\B|\Bei\B|\Bai\B|\Bph\B|\Bia\B|\Bck\B|\Buv\B
+                                                     \Baa\b|\Bou\b|\Bau\b|\Baw\b|\Bav\b|\Bow\b|\Bov\b|\Bee\b|\Bii\b|\Bea\b|\Bei\b|\Bai\b|\Bph\b|\Bia\b|\Bck\b|\Buv\b
+                                                     \baa\B|\bou\B|\bau\B|\baw\B|\bav\B|\bow\B|\bov\B|\bee\B|\bii\B|\bea\B|\bei\B|\bai\B|\bph\B|\bia\B|\bck\B|\buv\B
+                                                     \baa\b|\bou\b|\bau\b|\baw\b|\bav\b|\bow\b|\bov\b|\bee\b|\bii\b|\bea\b|\bei\b|\bai\b|\bph\b|\bia\b|\bck\b|\buv\b");
         public static Regex regSingle = new Regex(@"[a,i,u,e,o,f,c,k,y]");
 
         //Evaluators to replace matching part of string.
@@ -30,6 +26,8 @@ namespace SoundexCore
                 case "iya": return "8";
                 case "der": return "9";
                 case "dra": return "9";
+                case "ush": return "7";
+                case "ish": return "7";
                 default: throw new Exception("Unexpected match!");
             }
         };
@@ -53,7 +51,6 @@ namespace SoundexCore
                 case "ei": return "2";
                 case "oo": return "3";
                 case "ph": return "6";
-                case "sh": return "7";
                 case "ia": return "8";
                 case "ck": return "k";
                 default: throw new Exception("Unexpected match!");
