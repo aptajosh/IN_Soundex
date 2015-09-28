74fcd470d9fff4676f6604ec1942c638c6fa587c
Sanket Joshi <sanketjoshi_2005@rediffmail.com>
2015-09-28 23:10:56 +0530

    added case for oosh
    ush ish and oosh must be match
    so vrushali and vrooshali will generate same reult

--- src/SoundexReEngineered/RegexHelp.cs
+++ src/SoundexReEngineered/RegexHelp.cs
@@ -10,14 +10,25 @@ namespace SoundexCore
     public static class RegexHelp
     {
         //Regex to find three letters match, two letters match, one letter match
-        public static Regex regTriples = new Regex(@"\Baou\B|\Biya\B|\Baou\b|\Biya\b|\baou\B|\biya\B|\baou\b|\biya\b|\Bish\B|\Bish\b|\bish\B|\bish\b|\bush\B|\bush\b|\Bush\b|\Bush\B|\Bder\b|\Bdra\b");
-        public static Regex regDoubles = new Regex(@"\Baa\B|\Bou\B|\Bau\B|\Baw\B|\Bav\B|\Bow\B|\Bov\B|\Bee\B|\Bii\B|\Bea\B|\Bei\B|\Bai\B|\Bph\B|\Bia\B|\Bck\B|\Buv\B
-                                                     \Baa\b|\Bou\b|\Bau\b|\Baw\b|\Bav\b|\Bow\b|\Bov\b|\Bee\b|\Bii\b|\Bea\b|\Bei\b|\Bai\b|\Bph\b|\Bia\b|\Bck\b|\Buv\b
-                                                     \baa\B|\bou\B|\bau\B|\baw\B|\bav\B|\bow\B|\bov\B|\bee\B|\bii\B|\bea\B|\bei\B|\bai\B|\bph\B|\bia\B|\bck\B|\buv\B
-                                                     \baa\b|\bou\b|\bau\b|\baw\b|\bav\b|\bow\b|\bov\b|\bee\b|\bii\b|\bea\b|\bei\b|\bai\b|\bph\b|\bia\b|\bck\b|\buv\b");
+        public static Regex regSquare = new Regex(@"\Boosh\B|\Boosh\b|\boosh\B|\boosh\b");
+        public static Regex regTriples = new Regex(@"\Baou\B|\Biya\B|\Baou\b|\Biya\b|\baou\B|\biya\B|\baou\b|\biya\b|\Bish\B|\Bish\b|\bish\B|\bish\b|\bush\B|\bush\b|\Bush\b|\Bush\B|\Bder\b|\Bdra\b|\B3sh\b|\B3sh\B|\B3h\b|\B3sh\b");
+        public static Regex regDoubles = new Regex(@"\Baa\B|\Bou\B|\Bau\B|\Baw\B|\Bav\B|\Bow\B|\Bov\B|\Bee\B|\Bii\B|\Bea\B|\Bei\B|\Bai\B|\Bph\B|\Bia\B|\Bck\B|\Buv\B|\Boo\B|
+                                                     \Baa\b|\Bou\b|\Bau\b|\Baw\b|\Bav\b|\Bow\b|\Bov\b|\Bee\b|\Bii\b|\Bea\b|\Bei\b|\Bai\b|\Bph\b|\Bia\b|\Bck\b|\Buv\b|\Boo\b|
+                                                     \baa\B|\bou\B|\bau\B|\baw\B|\bav\B|\bow\B|\bov\B|\bee\B|\bii\B|\bea\B|\bei\B|\bai\B|\bph\B|\bia\B|\bck\B|\buv\B|\boo\B|
+                                                     \baa\b|\bou\b|\bau\b|\baw\b|\bav\b|\bow\b|\bov\b|\bee\b|\bii\b|\bea\b|\bei\b|\bai\b|\bph\b|\bia\b|\bck\b|\buv\b|\boo\b");
         public static Regex regSingle = new Regex(@"[a,i,u,e,o,f,c,k,y]");
 
         //Evaluators to replace matching part of string.
+        public static MatchEvaluator evalSquare = match =>
+        {
+            switch (match.Value)
+            {
+                case "oosh": return "7";
+                default: throw new Exception("Unexpected match!");
+            }
+        };
+        
+        
         public static MatchEvaluator evalTriples = match =>
         {
             switch (match.Value)
@@ -28,6 +39,7 @@ namespace SoundexCore
                 case "dra": return "9";
                 case "ush": return "7";
                 case "ish": return "7";
+                case "3sh": return "7";
                 default: throw new Exception("Unexpected match!");
             }
         };
