4f358f571524345e23944f2581da6942cf55538c
Sanket Joshi <sanketjoshi_2005@rediffmail.com>
2015-09-29 17:28:18 +0530

    removed unwanted code from RegexTriples

--- src/SoundexReEngineered/RegexHelp.cs
+++ src/SoundexReEngineered/RegexHelp.cs
@@ -11,7 +11,7 @@ namespace SoundexCore
     {
         //Regex to find three letters match, two letters match, one letter match
         public static Regex regSquare = new Regex(@"\Boosh\B|\Boosh\b|\boosh\B|\boosh\b");
-        public static Regex regTriples = new Regex(@"\Baou\B|\Biya\B|\Baou\b|\Biya\b|\baou\B|\biya\B|\baou\b|\biya\b|\Bish\B|\Bish\b|\bish\B|\bish\b|\bush\B|\bush\b|\Bush\b|\Bush\B|\Bder\b|\Bdra\b|\B3sh\b|\B3sh\B|\B3h\b|\B3sh\b");
+        public static Regex regTriples = new Regex(@"\Baou\B|\Biya\B|\Baou\b|\Biya\b|\baou\B|\biya\B|\baou\b|\biya\b|\Bish\B|\Bish\b|\bish\B|\bish\b|\bush\B|\bush\b|\Bush\b|\Bush\B|\Bder\b|\Bdra\b");
         public static Regex regDoubles = new Regex(@"\Baa\B|\Bou\B|\Bau\B|\Baw\B|\Bav\B|\Bow\B|\Bov\B|\Bee\B|\Bii\B|\Bea\B|\Bei\B|\Bai\B|\Bph\B|\Bia\B|\Bck\B|\Buv\B|\Boo\B|
                                                      \Baa\b|\Bou\b|\Bau\b|\Baw\b|\Bav\b|\Bow\b|\Bov\b|\Bee\b|\Bii\b|\Bea\b|\Bei\b|\Bai\b|\Bph\b|\Bia\b|\Bck\b|\Buv\b|\Boo\b|
                                                      \baa\B|\bou\B|\bau\B|\baw\B|\bav\B|\bow\B|\bov\B|\bee\B|\bii\B|\bea\B|\bei\B|\bai\B|\bph\B|\bia\B|\bck\B|\buv\B|\boo\B|
@@ -39,7 +39,6 @@ namespace SoundexCore
                 case "dra": return "9";
                 case "ush": return "7";
                 case "ish": return "7";
-                case "3sh": return "7";
                 default: throw new Exception("Unexpected match!");
             }
         };
